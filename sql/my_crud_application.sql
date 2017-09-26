08a8c0ea66d13bbd5aec162775401e67093c48c4
pramodb-iprogrammer <pramodb@iprogrammer.com>
2017-09-26 18:47:01 +0530

    modified sql file

--- sql/my_crud_application.sql
+++ sql/my_crud_application.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.16  Distrib 10.2.7-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.15  Distrib 10.0.31-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: my_crud_application
 -- ------------------------------------------------------
--- Server version	10.2.7-MariaDB-10.2.7+maria~trusty-log
+-- Server version	10.0.31-MariaDB-0ubuntu0.16.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -28,12 +28,13 @@ CREATE TABLE `login_details` (
   `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `createdatetime` datetime NOT NULL,
-  `lastupdateddatetime` datetime DEFAULT NULL,
-  `lastlogin` datetime DEFAULT NULL,
+  `lastupdateddatetime` datetime NOT NULL,
+  `lastlogin` datetime NOT NULL,
+  `$remember_me` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_620B15F7E7927C74` (`email`),
   UNIQUE KEY `UNIQ_620B15F7F85E0677` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,8 +43,38 @@ CREATE TABLE `login_details` (
 
 LOCK TABLES `login_details` WRITE;
 /*!40000 ALTER TABLE `login_details` DISABLE KEYS */;
-INSERT INTO `login_details` VALUES (2,'pramod@iprogrammer.com','pramod','ZA==','2017-09-08 19:30:02',NULL,NULL);
+INSERT INTO `login_details` VALUES (1,'pramodb@iprogrammer.com','pramod','pramod123','2017-09-18 14:48:58','0000-00-00 00:00:00','0000-00-00 00:00:00',0);
 /*!40000 ALTER TABLE `login_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` double NOT NULL,
+  `first_created` datetime NOT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_B3BA5A5A5E237E06` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'iPhone','qwerty','6adf82f78c63fb152548b94eb6a0d158.png',123,'2017-09-26 15:09:26','2017-09-26 17:30:56'),(2,'Android','Android 786','470f24bf25a5d5bc926c2f4fb9909f7c.gif',786,'2017-09-26 17:35:11',NULL),(3,'Imaginary product','sss','c52c0306de67567937728e8e2502f3e0.png',123,'2017-09-26 17:50:32',NULL);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -55,4 +86,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-08 20:09:47
+-- Dump completed on 2017-09-26 18:46:24
